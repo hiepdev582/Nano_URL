76238cb6e7d9a5279ea7e54b8a1029faa459488a
hiepdev582 <hiepdev582@gmail.com>
2026-08-21 10:19:00 +0700

    chore: complete config frontend & backend

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,4 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
+  runtimeConfig: {
+    apiServerUrl: process.env.NUXT_API_SERVER_URL || "http://nginx:80",
+
+    public: {
+      apiClientUrl: process.env.NUXT_PUBLIC_API_CLIENT_URL || "",
+    },
+  },
 });
